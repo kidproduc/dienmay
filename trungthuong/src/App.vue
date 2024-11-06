6d0a76d60f7804d1d60290fcce01e5a836a8ce23
Pham Cao Ky <kyptit94@gmail.com>
2024-11-06 16:15:20 +0700

    init

---
+++ trungthuong/src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import LuckyWheel from './components/LuckyWheel.vue'
+import LuckyPrize from './components/LuckyPrize.vue';
+import LuckyHeader from './components/LuckyHeader.vue';
+
+import { NGrid, NGi } from 'naive-ui'
+export default {
+  components: {
+    LuckyWheel,
+    LuckyPrize,
+    NGrid,
+    NGi,
+    LuckyHeader
+  }
+}
+</script>
+
+<template>
+  <LuckyHeader />
+  <n-grid x-gap="12" :cols="2">
+    <n-gi>
+      <LuckyPrize firstPlace="First" secondPlace="Second" thirdPlace="Third" />
+    </n-gi>
+    <n-gi>
+      <LuckyWheel />
+    </n-gi>
+  </n-grid>
+</template>
+
+<style scoped></style>
---
+++ trungthuong/src/components/LuckyHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <n-layout-header>
+        <img src="https://i.gyazo.com/d936cdcc28e1f6d50c8b30eef7255d3d.png" height="50" />
+    </n-layout-header>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { NLayoutHeader, NSpace, NAvatar, NH1, NButton } from 'naive-ui';
+
+export default defineComponent({
+    name: 'LuckyHeader',
+    components: {
+        NLayoutHeader,
+        NSpace,
+        NAvatar,
+        NH1,
+        NButton
+    }
+});
+</script>
+
+<style scoped>
+.n-layout-header {
+    background-color: #009ee1;
+    padding: 16px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+</style>
